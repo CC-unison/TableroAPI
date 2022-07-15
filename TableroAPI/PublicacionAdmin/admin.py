ac527d659965c032e85f98597486006c88aa4744
Sebastian Rivera <sebastan12grc@gmail.com>
2022-07-15 13:38:03 -0700

    Se creo el/la Modelo/Tabla de Publicaciones_Noticias. Se modifico los foreign keyes de los modelos en Admin para solo guardar el id en vez del objeto.

--- TableroAPI/PublicacionAdmin/admin.py
+++ TableroAPI/PublicacionAdmin/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from PublicacionAdmin.models import Publicaciones_Admins
+from PublicacionAdmin.models import Publicaciones_Admins, Publicaciones_Noticias
 
 # Register your models here.
 
 class PublicacionAdminAdmin(admin.ModelAdmin):
     pass
 admin.site.register(Publicaciones_Admins, PublicacionAdminAdmin)
+
+class PublicacionNoticiaAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Publicaciones_Noticias, PublicacionNoticiaAdmin)
